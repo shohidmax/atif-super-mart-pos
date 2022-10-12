4a7e9032b8372b155bc9bba9fcdd712aa87eb38a
shohdmax <shohdmax@gmail.com>
2022-10-13 03:53:31 +0600

    progress added

--- src/Component/Inventry/Updatedamage/Updatedamage.js
+++ src/Component/Inventry/Updatedamage/Updatedamage.js
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from 'react';
 const Updatedamage = () => {
     const [Pdata, setPdata] = useState([]);
 
-
     const findproduct = (B) =>{
         B.preventDefault();
         const Find = B.target.bangla.value;
-        const url = `http://localhost:5000/damage-stock-update/${Find}`;
+        const url = `https://frozen-badlands-76581.herokuapp.com/damage-stock-update/${Find}`;
         console.log(url);
         fetch(url, {
             method: 'GET',
@@ -33,7 +32,7 @@ const Updatedamage = () => {
         const reStock = parseFloat(Damage_Quntity) + Pdata.Damage_Quntity;
         const updatedStock = {reStock};
  
-        const url = `http://localhost:5000/handleAddToDamage/${ids}`;
+        const url = `https://frozen-badlands-76581.herokuapp.com/handleAddToDamage/${ids}`;
         fetch(url, {
             method: 'PUT',
             headers: {
@@ -58,7 +57,7 @@ const Updatedamage = () => {
         const reStock = parseFloat(Damage_Quntity) - Pdata.Damage_Quntity;
         const updatedStock = {reStock};
  
-        const url = `http://localhost:5000/handleAddToDamage/${ids}`;
+        const url = `https://frozen-badlands-76581.herokuapp.com/handleAddToDamage/${ids}`;
         fetch(url, {
             method: 'PUT',
             headers: {
@@ -99,7 +98,10 @@ const Updatedamage = () => {
     //     "Comment": 0,
     //     "Status": "active"
     // }
-
+    if (Pdata.length == 0) {
+    
+        
+    }
 
 
 
@@ -113,53 +115,88 @@ const Updatedamage = () => {
                 <div>
                     <h2 className='online'>Find Product {Pdata.length}</h2>
                     <form onSubmit={findproduct}> 
-                        <input name="bangla"   className="input input-bordered input-info w-full max-w-sm m-2"   type="text" placeholder='Product Stock Quntity' required />
+                        <input name="bangla"   className="input input-bordered input-secondary w-full max-w-xs m-2 p-2"   type="text" placeholder='Enter Your BarCode Hare' required />
                         
                     </form>
-
+ 
                 </div>
-                <div className='flex p-3 m-2 text-left mx-auto'>
-                    <div>
-                    <h2 className=''>Name : {Pdata.Brand}{" "}{Pdata.Product}{" "} {Pdata.Style}</h2>
+                {
+                    Pdata.length == 0 ? <div>
+                        <div class="border border-blue-300 shadow rounded-md    w-full max-w-xs m-2 p-2 mx-auto">
+                            <div class="animate-pulse flex space-x-4">
+                              <div class="rounded-full bg-slate-200 h-10 w-10"></div>
+                              <div class="flex-1 space-y-6 py-1">
+                                <div class="h-2 bg-slate-200 rounded"></div>
+                                <div class="space-y-3">
+                                  <div class="grid grid-cols-3 gap-4">
+                                    <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                                    <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                                  </div>
+                                  <div class="h-2 bg-slate-200 rounded"></div>
+                                </div>
+                              </div>
+                            </div>
+                            </div>
+
+
+                    </div> : <div className=' Flex sm:flex   p-3 m-2 text-left mx-auto'>
+                    <div className='mx-auto'>
+                    <h2 className=''>Product : {Pdata.Brand}{" "}{Pdata.Product}{" "} {Pdata.Style}</h2>
+                    <hr/>
                     <h2 className=''>Stock : {Pdata.StockQty}</h2>
-                    <h2>Suppler : {Pdata.Supplier_Name} Ltd.</h2>
-                    <h2>Damage : {Pdata.Damage_Quntity} Ltd.</h2>
-                    <h1>Product Barcode : {Pdata.BarCode}</h1>
-                    <input type="radio" name="radio-4" class="radio radio-accent" checked />
-
-
+                    <hr/>
+                    <h2 className='text-1xl'>Suppler : {Pdata.Supplier_Name} </h2>
+                    <hr/>
+                    <h2 className='text-red-500 text-2xl'>Damage : {Pdata.Damage_Quntity} Pcs</h2>
+                    <hr className='order'/>
                     </div>
                     <div>
                         <hr/>
                     </div>
-                    <div className=' mx-auto'>
-                    <h2 className=''>Name : {Pdata.Brand}{" "}{Pdata.Product}{" "} {Pdata.Style}</h2>
-                    <h2 className=''>Stock : {Pdata.StockQty}</h2>
-                    <h2>Suppler : {Pdata.Supplier_Name} Ltd.</h2>
+                    <div className='  mx-auto'>
+                    <h2 className=''>CPU Value ৳ : {Pdata.CPU}</h2>
+                    <hr/>
+                    <h2 className=''>RPU Value : {Pdata.RPU}</h2>
+                    <hr/>
+                    <h2>Status : {Pdata.Status} </h2>
+                    <hr/>
+                    <h1>Product _id: {Pdata._id}</h1>
+                    <hr/>
+                    
                     <h1>Product Barcode : {Pdata.BarCode}</h1>
-
+                    <hr/>
                     </div>
                 </div>
+                }
             
 
             {/* <button onClick={handleDelever}  className='btn btn-danger w-25 mt-2'> Delevery</button> */}
             </div>
-                <div className='flex '>
-                    <div className='border border-primary rounded m-3 w-2/4'>
+            {
+                !Pdata.length == 0 ? 
+            
+                <div className='Flex sm:flex '>
+                    <div className='border border-primary rounded m-3 sm:w-2/4'>
                         <form className='m-2' onSubmit={handleAddToDamage}>
-                        <input  className="input input-bordered input-info w-full max-w-sm m-2"   type="number" name="Stock_Qty" placeholder='Product Stock Quntity' required />
-                        <input className="form-submit  btn btn-primary w-100 mb-2" type="submit" value="Add TO Damage"/>
+                        <input  className="input input-bordered input-info w-full max-w-sm m-2" autoComplete='off'   type="number" name="Stock_Qty" placeholder='Product Stock Quntity' required />
+                        <input className="form-submit  btn btn-warning w-100 mb-2" type="submit" value="Add TO Damage"/>
 
                         </form>
                     </div> 
-                    <div className='border border-primary rounded m-3 w-2/4'>
+                    <div className='border border-primary rounded m-3 sm:w-2/4'>
                         <form className='m-2' onSubmit={handleRemoveDamage}>
-                            <input  className="input input-bordered input-info w-full max-w-sm m-2"   type="number" name="Stock_Qty" placeholder='Product Stock Quntity' required />
-                            <input className="form-submit  btn btn-primary w-100 mb-2" type="submit" value="Add Replac Stock"/>
-
+                            <input  className="input input-bordered input-info w-full max-w-sm m-2" autoComplete='off'   type="number" name="Stock_Qty" placeholder='Product Stock Quntity' required />
+                            {
+                               Pdata.Damage_Quntity == 0? <input className="form-submit  btn btn-error hover:bg-sky-700  text-white w-100 mb-2" disabled type="submit" value="Add Replac Stock"/>: <input className="form-submit  btn btn-error hover:bg-sky-700  text-white w-100 mb-2" type="submit"  value="Add Replac Stock"/>
+                            }
+  
                         </form>
                     </div>
+                </div> : <div><progress class="progress progress-info bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  w-full max-w-xs  mx-auto"></progress>  
+
+
                 </div>
+                }
              </div>
             
         </div>
--- src/Component/Pages/Header/Header.js
+++ src/Component/Pages/Header/Header.js
@@ -11,18 +11,22 @@ const Header = ({Supplier, Brand, Product, Groups, Products}) => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><Link to="/home">Sales</Link></li>
+        
         <li tabindex="0">
           <Link to="/home" className="justify-between">
-            Parent
             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
           </Link>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><Link to="/addssr">lolex</Link></li>
+          
+          </li>
+          <li className='btn btn-primary p-2 m-2 '><Link to="/sales">Sales</Link></li>
+          <li className='btn btn-primary p-2 m-2 '><Link to="/damageq">Damage</Link></li>
+          <li className='btn btn-primary p-2 m-2 '> <Link to="/Allmanage">Allmanage</Link></li>
+          <li className='btn btn-primary p-2 m-2  '> <input type="radio" name="radio-1" class="radio radio-accent"  checked/>{Brand.length} </li>
+          <li  className='btn btn-primary p-2 m-2 '> <input type="radio" name="radio-2" class="radio radio-accent" checked />{Supplier.length} </li>
+          <li  className='btn btn-primary p-2 m-2 '> <input type="radio" name="radio-3" class="radio radio-accent" checked />{Product.length} </li>
+          <li  className='btn btn-primary p-2 m-2 '> <input type="radio" name="radio-6" class="radio radio-accent" checked />{Groups.length} </li>
+          <li  className='btn btn-primary p-2 m-2 '> <input type="radio" name="radio-5" class="radio radio-accent" checked />{Products.length} </li>
+      
       </ul>
     </div>
     <Link to="/home" className="btn btn-ghost normal-case text-xl">ATIF SUPER MART</Link>
@@ -30,26 +34,25 @@ const Header = ({Supplier, Brand, Product, Groups, Products}) => {
   <div className="navbar-end hidden lg:flex">
     <ul className="menu menu-horizontal ml-3">
      
-      <li> <input type="radio" name="radio-1" class="radio radio-accent"  checked/>{Brand.length} </li>
-      <li> <input type="radio" name="radio-2" class="radio radio-accent" checked />{Supplier.length} </li>
-      <li> <input type="radio" name="radio-3" class="radio radio-accent" checked />{Product.length} </li>
-      <li> <input type="radio" name="radio-6" class="radio radio-accent" checked />{Groups.length} </li>
-      <li> <input type="radio" name="radio-5" class="radio radio-accent" checked />{Products.length} </li>
-      
       
       <li tabindex="0">
-        <a>
+        {/* <a>
           Parent
           <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul className="p-2">
-          <li><a>Submenu 1</a></li>
-          <li><Link to="/addssr">lolex</Link></li>
-        </ul>
+        </a> */}
+        
       </li>
+      <li className='btn  btn-primary p-2 m-2 '> <input type="radio" name="radio-1" class="radio radio-accent"  checked/>{Brand.length} </li>
+      <li  className='btn btn-primary p-2 m-2 '> <input type="radio" name="radio-2" class="radio radio-accent" checked />{Supplier.length} </li>
+      <li  className='btn btn-primary p-2 m-2 '> <input type="radio" name="radio-3" class="radio radio-accent" checked />{Product.length} </li>
+      <li  className='btn btn-primary p-2 m-2 '> <input type="radio" name="radio-6" class="radio radio-accent" checked />{Groups.length} </li>
+      <li  className='btn btn-primary p-2 m-2 '> <input type="radio" name="radio-5" class="radio radio-accent" checked />{Products.length} </li>
       
-      <li> <Link to="/addssr">add sr</Link></li>
-      <li> <Link to="/Allmanage">Allmanage</Link></li>
+      
+      
+      <li className='btn btn-primary p-2 m-2 '><Link to="/sales">Sales</Link></li>
+      <li className='btn btn-primary p-2 m-2 '><Link to="/damageq">Damage</Link></li>
+      <li className='btn btn-primary p-2 m-2 '> <Link to="/Allmanage">Allmanage</Link></li>
     </ul>
   </div>
   

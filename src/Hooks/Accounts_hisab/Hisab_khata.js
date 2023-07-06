ab9bfb79b1b77b51e75e6daad9eb5400614473fa
Sarwar Jahan <shohidmax@gmail.com>
2023-07-06 18:10:28 +0600

    bangla

--- src/Hooks/Accounts_hisab/Hisab_khata.js
+++ src/Hooks/Accounts_hisab/Hisab_khata.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+const api = 'api/v3/finalsubmit';
 const Hisab_khata = () => {
     const [hisab_summ, setHisab] = useState([]);
+    useEffect( ()=>{
+        fetch(`https://atifsupermart.onrender.com/${api}`)
+        .then(res => res.json())
+        .then(data => {
+            setaddMoney(data);
+            console.log(data); 
+        });
+    }, [hisab_summ]);
     return ([hisab_summ, setHisab]);
 };
 
